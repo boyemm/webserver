03a203a30031d6eaa551df3789c5e943a9773ead
Boye Mathias Molteberg <bmm@hotmail.no>
2023-08-10 14:24:37 +0200

    init

---
+++ launch.py
@@ -0,0 +1,9 @@
+import altair as alt
+from fastapi import FastAPI, Query, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
+
+
+
+#Se assignment 5 in IN4110
